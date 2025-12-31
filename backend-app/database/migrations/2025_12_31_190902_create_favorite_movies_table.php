5151efe1279095b9e0b2c8ae3e3bff5a61af3241
Koonac <rick.plays@hotmail.com>
2025-12-31 16:16:10 -0300

    feat: :sparkles: table favorite_movies

---
+++ backend-app/database/migrations/2025_12_31_190902_create_favorite_movies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favorite_movies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+			$table->string('tmdb_id');
+			$table->boolean('adult')->default(false);
+			$table->string('original_language')->nullable();
+			$table->string('original_title')->nullable();
+			$table->string('title')->nullable();
+			$table->string('overview')->nullable();
+			$table->string('backdrop_path')->nullable();
+			$table->string('poster_path')->nullable();
+			$table->string('release_date')->nullable();
+			$table->float('popularity')->nullable();
+			$table->float('vote_average')->nullable();
+			$table->integer('vote_count')->nullable();
+			$table->json('genres')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favorite_movies');
+    }
+};
